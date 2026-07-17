48d1834f9d857e82c6cd46ad625655e2560d32f7
BrayanMarcelo <161670688+BrayaMarceLord@users.noreply.github.com>
2026-07-16 22:59:23 -0500

    Remove UART driver include from main.c

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include "2_HAL/lcd_i2c.h"
 #include "5_Game/game.h"
 #include "5_Game/buzzer.h"
-#include "1_Drivers/Driver_UART.h"
+
 
 // ---- Capa del profesor ----
 display16x32_t myDisplay = {
